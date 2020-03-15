6e44542f0c64704b7b09541e41be1a0b29b669b2
luojia65 <me@luojia.cc>
2020-03-15 18:53:25 +0800

    Abstract pollard rho algorithm

--- src/bin/pollard-rho-abstract.rs
+++ src/bin/pollard-rho-abstract.rs
@@ -93,16 +93,76 @@ fn miller_rabin<T: MillerRabin + Clone>(n: &T) -> bool {
 }
 
 impl MillerRabin for usize {
-    fn is_simple_prime(&self) -> bool { *self == 2 || *self == 3 }
-    fn is_simple_composite(&self) -> bool { self % 2 == 0 || self % 3 == 0 }
+    fn is_simple_prime(&self) -> bool { 
+        *self == 2 || *self == 3 || *self == 5 || *self == 7 
+    }
+    fn is_simple_composite(&self) -> bool { 
+        self % 2 == 0 || self % 3 == 0 ||
+        self % 5 == 0 || self % 7 == 0 
+    }
     fn dec(&self) -> Self { self - 1 }
     fn simple_primes() -> &'static [Self] { &[0, 2, 7, 61] }
     fn eq(&self, other: &Self) -> bool { self == other }
     fn rem(&self, other: &Self) -> Self { self % other }
 }
 
+fn modmuladd<T: ModPow>(a: T, b: T, c: &T, m: &T) -> T {
+    let mut ans = modmul(a, b, &m);
+    ans.add_assign(&c);
+    ans.rem_assign(&m);
+    ans
+}
+
+trait PollardRho: MillerRabin {
+    fn rand_region(limit: &Self) -> Self;
+    fn gcd(a: &Self, b: &Self) -> Self;
+    fn diff(a: &Self, b: &Self) -> Self;
+    fn max(a: &Self, b: &Self) -> Self;
+    fn div(&self, other: &Self) -> Self;
+}
+
+fn pollard_rho<T: PollardRho + Clone>(n: &T, mut pre: T) -> T {
+    if miller_rabin(n) {
+        return T::max(n, &pre);
+    }
+    let mut t1 = T::rand_region(&n);
+    let b = T::rand_region(&n);
+    let mut t2 = modmuladd(t1.clone(), t1.clone(), &b, &n);
+    while !t1.eq(&t2) {
+        let t = T::gcd(&T::diff(&t1, &t2), &n);
+        if !t.eq(&T::identity()) && !t.eq(&n) {
+            pre = pollard_rho(&t, pre);
+            pre = pollard_rho(&n.div(&t), pre);
+        }
+        t1 = modmuladd(t1.clone(), t1, &b, &n);
+        t2 = modmuladd(t2.clone(), t2, &b, &n);
+        t2 = modmuladd(t2.clone(), t2, &b, &n);
+    }
+    pre
+}
+
+impl PollardRho for usize {
+    fn rand_region(limit: &Self) -> Self { 
+        rand::random::<usize>() % (limit - 1) + 1 
+    }
+    fn gcd(a: &Self, b: &Self) -> Self {     
+        if *b == 0 { *a } else { Self::gcd(b, &(a % b)) } 
+    }
+    fn diff(a: &Self, b: &Self) -> Self {
+        if a > b { a - b } else { b - a }
+    }
+    fn max(a: &Self, b: &Self) -> Self {
+        if a > b { *a } else { *b }
+    }
+    fn div(&self, other: &Self) -> Self {
+        self / other
+    }
+}
+
 fn main() {
-    println!("{}", modpow(2, 10000, &1007)); // 929
-    println!("{}", miller_rabin(&2)); // true
-    println!("{}", miller_rabin(&4)); // false
+    let mut a = 0;
+    while a == 0 {
+        a = pollard_rho(&1120000, a);
+        println!("{:?}", a);
+    }
 }
